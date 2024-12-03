e7ad3fbe515ab35a1d2961df97e899d9fcdf6463
Frank S <Frank@test.com>
2024-12-02 20:57:41 -0800

    asd

--- s3a.tf
+++ s3a.tf
@@ -1,7 +1,16 @@
 how to modify a remote file:
+add a line to s3a.tf
+git add .\s3a.tf
+git commit
+[master a0421d5] adf
+ 1 file changed, 1 insertion(+)
+git push
+that's it
 1
 2
 3
+4
+
 git add .\s3a.tf
 git commit
 [master 359e8c5] adsfsa
